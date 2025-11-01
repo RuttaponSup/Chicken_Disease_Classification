1cf49038014f486debb8e43ab64bbd4de8439b60
RuttaponSup <kenruttapon@gmail.com>
2025-11-01 14:05:29 +0700

    Data ingestion

---
+++ main.py
@@ -0,0 +1,13 @@
+from Chicken_Disease_Classifier import logger
+from Chicken_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
